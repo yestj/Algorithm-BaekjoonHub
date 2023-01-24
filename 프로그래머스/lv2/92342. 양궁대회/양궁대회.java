b7eff6017de6941fa0f7714ba5134dc2a8f8488b
Yezi Heo <104667483+yestj@users.noreply.github.com>
2023-01-24 22:08:18 +0900

    [level 2] Title: 양궁대회, Time: 4.53 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -1,7 +1,9 @@
-class Solution {
-   static int[] appeachShots; // 어피치가 각 과녘에 몇 발씩 쐈는지 저장
+public class Solution {
+
+    
+    static int[] appeachShots; // 어피치가 각 과녘에 몇 발씩 쐈는지 저장
     static int[] ryanShots = new int[11]; // 라이언이 각 과녘에 몇 발씩 쐈는지 저장
-    static int maxDiff = -1; //
+    static int maxDiff = -1; // 가장 큰 차이 
 
     static int[] answer = {-1}; // 결과값 저장
 
@@ -15,30 +17,34 @@ class Solution {
 
     static void dfs(int cnt, int n) {
 
-        if(cnt == n + 1){
+				// 더이상 쏠 화살이 없으면 현재까지 점수를 계산해줌.
+        if (cnt == n + 1) {
             int appeachScore = 0;
             int ryanScore = 0;
-            for(int i = 0; i < 11; i++) {
-                if(appeachShots[i] != 0 || ryanShots[i] != 0) {
-                    if(appeachShots[i] >= ryanShots[i]){
+            for (int i = 0; i < 11; i++) {
+                if (appeachShots[i] != 0 || ryanShots[i] != 0) {
+                    if (appeachShots[i] >= ryanShots[i]) {
                         appeachScore += 10 - i;
                     } else {
-                        ryanScore += 10-i;
+                        ryanScore += 10 - i;
                     }
                 }
             }
-            if(ryanScore > appeachScore && ryanScore - appeachScore >= maxDiff) {
+						// 계산한 결과 라이언이 우승인 경우에 answer배열을 업데이트 해 줌
+            if (ryanScore > appeachScore && ryanScore - appeachScore >= maxDiff) {
                 answer = ryanShots.clone();
                 maxDiff = ryanScore - appeachScore;
             }
             return;
         }
-
+				
+				// 완탐으로 하나씩 늘려가면서 검사하게 함
         for (int i = 0; i < 11 && ryanShots[i] <= appeachShots[i]; i++) {
             ryanShots[i]++;
-            dfs(cnt+1, n);
+            dfs(cnt + 1, n);
             ryanShots[i]--;
         }
 
     }
+
 }
